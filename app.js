80a35b8de3993d0caaf3b374d617f4672f041eff
Oghenero Okerri <oghenerookerri@gmail.com>
2022-12-16 13:50:34 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const session = require('express-session');
+const connectFlash = require('connect-flash');
+
+const port = 3000;
+
+const app = express();
+
+app.set('view engine', 'ejs')
+
+// use express-session
+app.use(session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(connectFlash());
+
+app.use((req,res,next)=>{
+    res.locals.messages = req.flash();
+    next()
+})
+
+
+app.get('/', (req,res)=>{
+  req.flash('error', 'this is an error');
+  req.flash('info', 'this is an error2');
+  req.flash('success', 'this is an error3')
+//   const messages = req.flash()
+//    console.log(messages)
+//     res.render('index', {messages})
+ res.redirect('/about')
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about')
+})
+
+app.listen(port, ()=>{
+    console.log(`listening for request at ${port}`)
+})
