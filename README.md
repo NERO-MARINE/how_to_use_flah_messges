985030920d3b34313a767c0e5106c1f6622b1f8d
Oghenero Okerri <oghenerookerri@gmail.com>
2022-12-28 22:11:12 +0100

    removed node_modules

---
+++ README.md
@@ -0,0 +1,1 @@
+# how_to_use_flash_messges in nodejs/expressjs!
--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // use express-session
+
 app.use(session({
-    secret: 'my secret',
+    secret: 'your own secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
 }))
 
+// use connect flash
 app.use(connectFlash());
 
 app.use((req,res,next)=>{
@@ -24,13 +26,17 @@ app.use((req,res,next)=>{
 
 
 app.get('/', (req,res)=>{
-  req.flash('error', 'this is an error');
-  req.flash('info', 'this is an error2');
-  req.flash('success', 'this is an error3')
-//   const messages = req.flash()
-//    console.log(messages)
-//     res.render('index', {messages})
- res.redirect('/about')
+   req.flash('error', 'there is an error1');
+   req.flash('success', 'you were successful1');
+   req.flash('success', 'you were successful2');
+   req.flash('success', 'you were successful3');
+   req.flash('error', 'there is an error2');
+   req.flash('info', 'you have been informed');
+
+//    const messages = req.flash()
+//    console.log(messages);
+//    res.render('index', {messages});
+  res.redirect('/about')
 })
 
 app.get('/about', (req,res)=>{
